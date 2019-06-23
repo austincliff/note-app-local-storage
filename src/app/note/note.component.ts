63cbc079a79600df8f6f31f12bb4bff5cd487070
Austin Cliff <ajcliff95@gmail.com>
2019-06-23 00:50:25 -0400

    added local storage to this copy of note app

--- src/app/note/note.component.ts
+++ src/app/note/note.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Note } from './note';
+import { setFirstTemplatePass } from '@angular/core/src/render3/state';
 
 @Component({
   selector: 'app-note',
@@ -10,14 +11,14 @@ export class NoteComponent implements OnInit {
 
   submittable = false;
 
-  note: Note = {
+  note = {
     title: '',
     description: '',
     id: 0,
     dateCreated: ''
   };
 
-  notes: Note[] = [];
+  notes = [];
 
   constructor() {
 
@@ -33,12 +34,29 @@ export class NoteComponent implements OnInit {
     return note = new Note(note.title, note.description, note.id, Date());
   }
 
+  deleteNote(i) {
+    let notes = this.notes.splice(i, 1);
+    return localStorage.setItem('notes', JSON.stringify(notes));
+  }
+
   onSubmit() {
     this.notes.push(this.createNote(this.note));
-    console.log(this.notes);
+  }
+
+  localStorage() {
+    localStorage.setItem('notes', JSON.stringify(this.notes));
+    return this.notes = JSON.parse(localStorage.getItem('notes'));
   }
 
   ngOnInit() {
+    this.notes = JSON.parse(localStorage.getItem('notes'));
   }
 
 }
+/*
+Turns a JSON object into a string, in this case the notes array
+localStorage.setItem('notes', JSON.stringify(this.notes));
+
+Turns a string into a JavaScript object
+getLocalStorage = JSON.parse(localStorage.getItem('notes'));
+*/
